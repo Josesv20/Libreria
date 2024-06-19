575175ccb925cdf697d7abf9a4aaab0181d14dfc
JOSE MARTIN SANCHEZ VARGAS <martinsv11@hotmail.com>
2024-06-19 14:54:40 -0500

    programacion con javascript

---
+++ app.js
@@ -0,0 +1,30 @@
+"use strict";
+
+function preguntar_y_escribir(pregunta) {
+    let respuesta = prompt(pregunta);
+    document.write(prefijo + respuesta);
+    return respuesta;
+}
+
+preguntar_y_escribir("Cual es tu nombre?", "Bienvenido ");
+led edad = preguntar_y_escribir("Cual es tu edad?", " de ");
+preguntar_y_escribir("Cual es tu correo?", "años y con correo electronico ");
+
+
+ //document.write("Bienvenidos a la Libreria Universal, donde encontraras tus libros favoritos,             ")
+        //let nombredeCliente = " ";
+        //nombredeCliente = prompt("Cual es tu nombre");
+        //let correoElectronico = "";
+        //correoElectronico = prompt("Cual es tu correo electronico");
+        //document.write("Hola " + nombredeCliente + " con correo eletronico " + correoElectronico);
+        //const edad = parseInt(prompt("Cual es tu edad?"));
+        //if (edad >= 60) {
+        //    document.write("<p>El ususario es adulto mayor y obtiene descuentos exclusivos</p>");
+        //            } else {
+        //                document.write("<p>El usuario no es adulto mayor</p>")
+        //            }
+        //if(edad < 18) {
+        //    document.write("<p>El usuario es menor de edad, recibe 20% de descuento en comics y mangas</p>");
+        //} else{
+        //    document.write("<p>El usuario es mayor de edad recibe 10% de descuento en Literatura Clasica</p>");
+        //}
